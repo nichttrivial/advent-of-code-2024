8776ac83c7ec881dad8d5f36d45ac651482d8972
Uwe Warschun <u.warschun@opcyc.de>
2024-11-21 18:42:06 +0000

    initial commit

---
+++ 1/hello.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+func main() {
+	var i int = 1
+	fmt.Println("Hello World Dev Container for AOC")
+	fmt.Println(i)
+	i = 2
+	fmt.Println(i)
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/nichttrivial/advent-of-code-2024
+
+go 1.23.2
